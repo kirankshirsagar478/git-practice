47fe1e978e642d49fe266888b627ac68eb38b57f
kirankshirsagar478 <kirankshirsagar478@gmail.com>
2021-10-25 18:10:47 +0530

    initial commit

---
+++ first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+
+        int a,b,c;
+        printf ("Enter first number:\n");
+        scanf ("%d",&a);
+        printf ("Enter second number:\n");
+        scanf("%d",&b);
+        c=a+b;
+        printf ("Addition is :%d\n",c);
+    
+    return 0;
+}
